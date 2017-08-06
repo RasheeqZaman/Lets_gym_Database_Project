80d14894f1e77eacebf393dc490aa7f04e3a7be9
IronmanBD <nahidul19967@gmail.com>
2017-08-06 18:44:11 +0600

    4 ta table add korsi

--- Rasheeq's Folder/lets_gym_db.sql
+++ Rasheeq's Folder/lets_gym_db.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.14, for Win64 (x86_64)
 --
--- Host: localhost    Database: lets_gym_database
+-- Host: localhost    Database: lets_gym_db
 -- ------------------------------------------------------
 -- Server version	5.7.14
 
@@ -14,6 +14,30 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `branch`
+--
+
+DROP TABLE IF EXISTS `branch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `branch` (
+  `b_id` int(11) NOT NULL,
+  `b_name` varchar(40) DEFAULT NULL,
+  `b_city` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`b_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch`
+--
+
+LOCK TABLES `branch` WRITE;
+/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
+/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `customer`
@@ -43,6 +67,32 @@ INSERT INTO `customer` VALUES (1,'Rasheeq Zaman','Dhaka','01734455817','zamanrsh
 UNLOCK TABLES;
 
 --
+-- Table structure for table `equipments`
+--
+
+DROP TABLE IF EXISTS `equipments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipments` (
+  `Equip_id` int(11) NOT NULL,
+  `Equip_name` varchar(50) DEFAULT NULL,
+  `Equip_type` varchar(50) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `cost` decimal(8,3) DEFAULT NULL,
+  PRIMARY KEY (`Equip_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipments`
+--
+
+LOCK TABLES `equipments` WRITE;
+/*!40000 ALTER TABLE `equipments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `membership`
 --
 
@@ -66,6 +116,58 @@ LOCK TABLES `membership` WRITE;
 /*!40000 ALTER TABLE `membership` DISABLE KEYS */;
 INSERT INTO `membership` VALUES (1,'Platimum',360,30000),(2,'Gold',180,18000),(3,'Silver',90,10000),(4,'Corporate',90,9000),(5,'Student',60,5000);
 /*!40000 ALTER TABLE `membership` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `owns`
+--
+
+DROP TABLE IF EXISTS `owns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `owns` (
+  `c_id` int(11) NOT NULL,
+  `m_id` int(11) NOT NULL,
+  `creat_date` date DEFAULT NULL,
+  `expiry_date` date DEFAULT NULL,
+  `status` char(5) DEFAULT NULL,
+  PRIMARY KEY (`c_id`,`m_id`),
+  KEY `m_id` (`m_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `owns`
+--
+
+LOCK TABLES `owns` WRITE;
+/*!40000 ALTER TABLE `owns` DISABLE KEYS */;
+/*!40000 ALTER TABLE `owns` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rents`
+--
+
+DROP TABLE IF EXISTS `rents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rents` (
+  `c_id` int(11) NOT NULL,
+  `equip_id` int(11) NOT NULL,
+  `checkout_date` date DEFAULT NULL,
+  PRIMARY KEY (`c_id`,`equip_id`),
+  KEY `equip_id` (`equip_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rents`
+--
+
+LOCK TABLES `rents` WRITE;
+/*!40000 ALTER TABLE `rents` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rents` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -101,4 +203,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-06 15:43:05
+-- Dump completed on 2017-08-06 18:33:55
